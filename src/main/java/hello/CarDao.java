c541f4f58d9bb80153890de1e7402557bd2374d4
Endre Midtgård Meckelborg <endre.midtgard.meckelborg@bring.com>
2019-01-21 16:50:18 +0100

    Add some real car names.

--- src/main/java/hello/CarDao.java
+++ src/main/java/hello/CarDao.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static java.util.Collections.emptyList;
-
 @Repository
 public class CarDao {
 
     public List<String> getCarNames() {
-        return emptyList();
+        return List.of("Honda", "Ford");
     }
 
 }
