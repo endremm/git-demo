d2904c6fd39cf0cf5182c654d49b48c87a2f7af0
Endre Midtgård Meckelborg <endre.midtgard.meckelborg@bring.com>
2019-01-22 10:46:11 +0100

    Add service for houses

---
+++ src/main/java/hello/HouseService.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HouseService {
+
+    private HouseDao houseDao;
+
+    @Autowired
+    public HouseService(HouseDao houseDao) {
+        this.houseDao = houseDao;
+    }
+
+}
